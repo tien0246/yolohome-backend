e400c3e4f054c2bc5e2610cd5624f73df78ade44
tien0246 <60417827+tien0246@users.noreply.github.com>
2025-03-20 16:09:41 +0700

    test

--- app/services/mqtt_service.py
+++ app/services/mqtt_service.py
@@ -1,5 +1,4 @@
 from app.core.mqtt_manager import get_mqtt_client
-import random
 class MQTTService:
     def __init__(self):
         self.client = get_mqtt_client()
@@ -9,6 +8,4 @@ class MQTTService:
     def stop(self):
         self.client.disconnect()
     def publish(self, feed_id, data):
-        value = random.randint(0, 100)
-        print('Publishing {0} to {1}.'.format(value, feed_id))
-        self.client.publish(feed_id, value)
+        self.client.publish(feed_id, data)
