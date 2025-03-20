a5e754835cfd6e2a6eca972c84c5a1dede451070
tien0246 <60417827+tien0246@users.noreply.github.com>
2025-03-20 15:17:38 +0700

    update

--- app/services/mqtt_service.py
+++ app/services/mqtt_service.py
@@ -1,4 +1,3 @@
-from app.utils.config import config
 from app.core.mqtt_manager import get_mqtt_client
 
 class MQTTService:
@@ -10,6 +9,6 @@ class MQTTService:
     def stop(self):
         self.client.disconnect()
     def publish(self, feed_id, data):
-        feed_path = f"{config.aio_username}/feeds/{feed_id}"
-        print(f"Publishing to {feed_path}: {data}")
-        self.client.publish(feed_path, str(data))
+        print(f"Publishing to {feed_id}: {data}")
+        feed = self.client.feeds(feed_id)
+        self.client.send_data(feed.key, data)
