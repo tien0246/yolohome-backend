f7402d8b8bea84c213d09ef10b29f9d9795e318e
tien0246 <60417827+tien0246@users.noreply.github.com>
2025-03-20 12:48:04 +0700

    move uuid to python

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, String, TIMESTAMP, func, text
+from sqlalchemy import Column, String, TIMESTAMP, func
 from app.db.session import Base
+import uuid
 
 class User(Base):
     __tablename__ = "User"
-    id = Column(String(36), primary_key=True, index=True, default=text("uuid()"))
+    id = Column(String(36), primary_key=True, index=True, default=uuid.uuid4)
     name = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, nullable=False, index=True)
     password = Column(String(255), nullable=False)
