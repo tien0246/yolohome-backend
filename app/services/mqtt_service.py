8998b389c9d96f442f02e6bc663329f73e8f9f9f
tien0246 <doantien541@gmail.com>
2025-04-22 18:12:42 +0700

    Update mqtt_service.py

--- app/services/mqtt_service.py
+++ app/services/mqtt_service.py
@@ -1,10 +1,18 @@
 from app.core.mqtt_manager import *
+import time
 class MQTTService:
     def __init__(self):
         self.client = get_mqtt_client()
     def start(self):
-        self.client.connect()
-        self.client.loop_background()
+        while True:
+            try:
+                self.client.connect()
+                self.client.loop_background()
+            except Exception as e:
+                print(f"Error connecting to MQTT broker: {e}")
+                # Retry connection after a delay
+                time.sleep(5)
+
     def stop(self):
         self.client.disconnect()
         print("MQTT client disconnected")
