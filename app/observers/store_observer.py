ab28dafa4184a837bfe26daba271407e9735ff20
tien0246 <doantien541@gmail.com>
2025-04-22 22:40:47 +0700

    Update store_observer.py

--- app/observers/store_observer.py
+++ app/observers/store_observer.py
@@ -11,6 +11,7 @@ class StoreObserver(IObserver):
         try:
             feed_id = data.get("feed_id")
             val = data.get("value")
+            print(f"Received data: {data}")
             device = session.query(Device).filter(Device.feed_id==feed_id).first()
             if device and val is not None:
                 print(f"Storing data for device {device.id}: {val}")
